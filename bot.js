417e2ebc7206afe7612a845d71396bf551acfd11
Gabethe64th <59423558+Gabethe64th@users.noreply.github.com>
2020-04-01 18:33:41 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1082,8 +1082,8 @@ bot.on('message', message=> {
             break;
 
             case 'pair':
-                if (message.mentions.users[1] != undefined){
-                    message.channel.send("*Reading the pair scores of "+message.mentions.users[0].username+" and "+message.mentions.users[1].username+"...*")
+                if (message.mentions.users.first(1) != undefined){
+                    message.channel.send("*Reading the pair scores of "+message.mentions.users.first().username+" and "+message.mentions.users.first(1).username+"...*")
                     pairscore = Math.floor ((Math.random() * 100));
                     message.channel.send("And the score is...**"+pairscore+"!**")
                 }
