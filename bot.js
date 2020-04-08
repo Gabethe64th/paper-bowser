486fe3edfa0377e42fcc68acb36eddd1e7297a33
Gabethe64th <59423558+Gabethe64th@users.noreply.github.com>
2020-04-08 15:01:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3293,7 +3293,7 @@ bot.on('messageReactionAdd', (messageReaction, user) => {
                 pc = cardspwp[pwpplayer];
                 for (i = 0; i < pc.length; i++){
                     if (message.content == pc[i]){
-                        othertype = message.content.slice(0, 2);
+                        othertype = args[0];
                         othernum = args[1];
                         checking = true;
                         checker = i;
