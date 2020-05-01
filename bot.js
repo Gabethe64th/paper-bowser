cb11234d48043504a5518e5cd773464455439d10
Gabethe64th <59423558+Gabethe64th@users.noreply.github.com>
2020-04-30 21:22:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1196,18 +1196,16 @@ bot.on('message', message=> {
                     vchannel = message.member.voiceChannelID;
                     message.member.voiceChannel.join().then(
                     connection => { 
+                        function playASong(connection){
                         vibes = 53;
                         vib = Math.floor (Math.random() * (vibes - 1 + 1)) +1;
                             
 
                 const dispatcher = connection.playFile('./vibes/vibes/tune (' + vib + ').mp3')
+                        }
 
-                dispatcher.on('finish', () => {
-                    vibes = 53;
-                    vib = Math.floor (Math.random() * (vibes - 1 + 1)) +1;
-                            
-
-                connection.playFile('./vibes/vibes/tune (' + vib + ').mp3')
+                dispatcher.on('end', () => {
+                    playASong(connection);
                 }) 
             })
                     
