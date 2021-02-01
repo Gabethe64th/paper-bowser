1999fbe241185983721232c7fe1a365b7af84066
Gabethe64th <59423558+Gabethe64th@users.noreply.github.com>
2021-02-01 00:36:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4607,6 +4607,7 @@ bot.on("message", async message => {
     if (args[1] == undefined) {
         vibepicked = Math.floor(Math.random() * vibesongs.length);
         console.log("Got vibe number " + vibepicked);
+        //oh
         songInfo = await ytdl.getInfo(vibesongs[vibepicked]);
         
     }
